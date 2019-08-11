847b9c33dabeca517d733777dbfe6efe293c90c8
kiugee <abhishekmazumdar118@gmail.com>
2019-08-11 16:20:44 +0530

    --postman checks successful--

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
+
 // @route---POST--api/users
 // @desc---Register a user
 //@access---Public
@@ -18,33 +18,12 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  async (req, res) => {
-    const err = validationResult(req);
-    if (!err.isEmpty()) {
-      return res.status(400).json({
-        ere: err.array()
-      });
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
     }
-    const { name, email, password } = req.body;
-
-    try {
-      let user = await User.findOne({ email });
-
-      if (user) {
-        return res.status(400).json({ msg: "User already exists" });
-      }
-      user = new User({ name, email, password });
-
-      const salt = await bcrypt.genSalt(10);
-
-      user.password = await bcrypt.hash(password, salt);
-
-      await user.save();
-      res.send("Register done..");
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
+    res.send("Passed...");
   }
 );
 
--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
-
 //ConnectDB
 connectDB();
-
 //init middleware
 app.use(express.json({ extended: false }));
-
 //InitialiseRoutes...
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`MagicHappens@${PORT}`));
