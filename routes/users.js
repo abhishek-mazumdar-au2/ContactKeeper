9b63877f1699b447fe5e8850f795272daa1bf43b
kiugee <abhishekmazumdar118@gmail.com>
2019-08-14 18:29:58 +0530

    **Add New Contact Route Working**

--- routes/users.js
+++ routes/users.js
@@ -24,7 +24,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
     const { name, email, password } = req.body;
 
@@ -62,7 +62,7 @@ router.post(
           res.json({ token });
         }
       );
-    } catch (error) {
+    } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
     }
